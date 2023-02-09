c5de8ccd629e4ce02f45b18c856482485c248f0b
Diana <taaldiana@gmail.com>
2023-02-09 14:40:35 +0200

    miks stiil peale ei lähe?

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import AddUser from "./components/users/AddUser";
+import UsersList from "./components/users/UsersList";
 
 function App() {
+    const users = [
+        {name: 'Kati',
+        age: 15
+        },
+        {
+            name: 'Mati',
+            age: 16
+        }
+
+    ]
   return (
       <div>
         <AddUser/>
+          <UsersList users={users}/>
       </div>
   );
 }
