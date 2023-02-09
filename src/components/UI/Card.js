c83e5046b334fd23479fd93738c0e0fc2e4a5560
Diana <taaldiana@gmail.com>
2023-02-09 12:08:21 +0200

    töötab!

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,12 +2,13 @@ import React from "react";
 import styles from './Card.module.css'
 
 const Card = (props) => {
-    return(
-        <div className=(`${styles.card}}
-        ${props.className}`)>
+    return (
+        <div className={`${props.className} ${styles.card}`}>
             {props.children}
         </div>
     )
 }
+
+
 
 export default Card
--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from './AddUser.module.css'
-import Card from './UI/Card'
+import Card from '../UI/Card'
 const AddUser = (props) => {
     const addUserHandler = (event) => {
         event.preventDefault()
